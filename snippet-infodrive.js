ce6de30278f808e95520b066074cf401af613996
Pramod Kumar <pramod.kumar2@spraxa.com>
2025-03-24 14:21:46 +0530

    #62926 - Optimize JSON Fetching in Cloudflare Snippets for Better Caching 1. Removed logic for reset cache.(If need in future, will add again).

--- snippet-infodrive.js
+++ snippet-infodrive.js
@@ -29,9 +29,6 @@ async function loadCacheData() {
 
 export default {
 	async fetch(request, response, next) {
-		if(new URL(request.url).pathname === '/reset-cache') {
-			cache.tradePorts = null;
-		}
 		await loadCacheData();
 		if (!cache.tradePorts) {
 			return new Response("Data not available.", { status: 500 });
